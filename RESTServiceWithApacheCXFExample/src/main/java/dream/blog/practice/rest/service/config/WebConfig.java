eb6418fa15a0cf7665adde76c2df01396f865abd
George Chou <george_chou@gss.com.tw>
2018-03-01 17:21:24 +0800

    Initial commit

---
+++ RESTServiceWithApacheCXFExample/src/main/java/dream/blog/practice/rest/service/config/WebConfig.java
@@ -0,0 +1,14 @@
+package dream.blog.practice.rest.service.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "dream.blog.practice.rest.service" })
+public class WebConfig implements WebMvcConfigurer {
+
+
+}
