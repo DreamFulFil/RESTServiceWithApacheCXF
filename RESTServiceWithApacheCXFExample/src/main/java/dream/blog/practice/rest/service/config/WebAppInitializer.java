1d5ad117933a41b1e2f470f4a390de59ccf0bcca
george_chou <george_chou@gss.com.tw>
2018-09-07 07:49:25 +0800

    加上註解為什麼可以用這個類別。

--- RESTServiceWithApacheCXFExample/src/main/java/dream/blog/practice/rest/service/config/WebAppInitializer.java
+++ RESTServiceWithApacheCXFExample/src/main/java/dream/blog/practice/rest/service/config/WebAppInitializer.java
@@ -2,6 +2,12 @@ package dream.blog.practice.rest.service.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+/**
+ * @author George-Chou
+ * 
+ * 從Servlet 3.0開始，專案內有實作ServletContainerInitializer的類別的話，
+ * 就等同於傳統的Java Web App在web.xml裡面對Web Container作設定。
+ */
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
